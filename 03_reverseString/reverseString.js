c4ff02e3cbf8954118e88b5bed1b97a5f4aa9aee
Alex <alex@moudry.net>
2024-09-26 15:22:01 -0500

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function (inputString)
+{
+    if (inputString.length <= 0)
+    {
+        return "";
+    }
+    let stringArray = [];
+    do
+    {
+        stringArray.push(inputString.at(0));
+        inputString = inputString.slice(1);
+    } while (inputString.length > 0)
 
+    let finalString = "";
+    stringArray.forEach((character) =>
+    { finalString = character + finalString; });
+
+    return finalString;
 };
 
 // Do not edit below this line
