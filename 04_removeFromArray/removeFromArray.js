6d9b4d85a9de6f4caeebb43359a9e9873dd2ea67
Alex <alex@moudry.net>
2024-10-01 10:57:21 -0500

    Implement untested 'removeFromArray'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (subjectArray, ...args)
+{
+    if (!subjectArray.isArray())
+    {
+        return "ERROR";
+    }
+    args.forEach((arg) =>
+    {
+        let index = subjectArray.indexOf(arg);
+        if (index > -1)
+        {
+            subjectArray.splice(index, 1);
+        }
+    })
+
+    return subjectArray;
 };
 
 // Do not edit below this line
