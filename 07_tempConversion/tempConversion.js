423b39ac5711ce1b758010b2d4bf4a450336d448
Alex <alex@moudry.net>
2024-10-02 11:17:17 -0500

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFar)
+{
+  let solution = (tempInFar - 32) * (5 / 9);
+  if (!Number.isInteger(solution))
+    solution = parseFloat(solution.toFixed(1));
+  return solution;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCel)
+{
+  let solution = (tempInCel * (9 / 5) + 32);
+  if (!Number.isInteger(solution))
+    solution = parseFloat(solution.toFixed(1));
+  return solution;
 };
 
 // Do not edit below this line
