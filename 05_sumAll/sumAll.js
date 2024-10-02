95812ff71d57f97c3e572e8360d5fb1f1fefabbb
Alex <alex@moudry.net>
2024-10-02 10:57:10 -0500

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (startInt, endInt)
+{
+    if (startInt < 0 || endInt < 0 || isNaN(startInt) || isNaN(endInt) || !Number.isInteger(startInt) || !Number.isInteger(endInt))
+        return "ERROR";
 
+    let sum = 0;
+    if (startInt <= endInt)
+    {
+        for (let i = startInt; i <= endInt; i++)
+        {
+            sum += i;
+        }
+    }
+    else
+    {
+        for (let i = endInt; i <= startInt; i++)
+        {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
