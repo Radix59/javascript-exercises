ef430f70a6a5775290a9bb59303b23f039561aec
Alex <alex@moudry.net>
2024-10-11 13:48:24 -0500

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (bookArray)
+{
+    return bookArray.map((item) =>
+    {
+        return item.title;
+    });
 };
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
+
+let booktitles = getTheTitles(books);
+console.log(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
