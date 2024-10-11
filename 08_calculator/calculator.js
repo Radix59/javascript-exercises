929a74a58c2152eea735192f8c73f545c86315fd
Alex <alex@moudry.net>
2024-10-11 11:17:14 -0500

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (a, b)
+{
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b)
+{
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (sumArray)
+{
+  return sumArray.reduce((sum, item) =>
+  {
+    return sum + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (multArray)
+{
+  return multArray.reduce((mult, item) =>
+  {
+    return mult * item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b)
+{
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a)
+{
+  let factorial = 1;
+  if (a > 0)
+  {
+    for (let i = 0; i < a; i++)
+    {
+      factorial *= (a - i);
+    }
+  }
+  return factorial;
 };
 
 // Do not edit below this line
