a1bab559bd9a8d6818f7e5c26f3971afcfe45e0b
Alex <alex@moudry.net>
2024-10-11 11:38:49 -0500

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (testWord)
+{
+    testWord = testWord.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    testWord = testWord.replaceAll(" ", "");
+    let testArray = [...testWord];
+    let isPalindrome = true;
+    for (let i = 0; i <= testArray.length / 2; i++)
+    {
+        if (testArray.at(i).toLowerCase() != testArray.at(-(i + 1)).toLowerCase())
+            isPalindrome = false;
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
