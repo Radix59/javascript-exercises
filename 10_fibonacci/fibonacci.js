a501e9331873da8f56e1af55807f03312bdf7609
Alex <alex@moudry.net>
2024-10-11 11:46:01 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (index)
+{
+    index = Number.parseInt(index);
+    if (isNaN(index) || index < 0)
+    {
+        return "OOPS";
+    }
+    let n1 = 0;
+    let n2 = 1;
+    let n3 = 0;
+    for (let i = 0; i < index; i++)
+    {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
+    return n1;
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
